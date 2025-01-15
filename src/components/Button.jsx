5f27546ba178f8276ac481f8c66818430fe097d4
LuizzJS <luizz.developer@gmail.com>
2025-01-15 16:58:25 +0100

    added default user image, improvements on button and profile components.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Button = ({ click, icon, label, type = "button", ...props }) => {
   return (
     <button
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
 import { checkIfLoggedIn, logout, API_URL } from "../auth";
 import { useNavigate, useLocation } from "react-router-dom";
-import { User, Mail, Clock, Briefcase, IdCard } from "lucide-react";
+import {
+  User,
+  Mail,
+  Clock,
+  Briefcase,
+  IdCard,
+  ArrowBigLeftDash,
+  CornerUpLeft,
+} from "lucide-react";
 import toast from "react-hot-toast";
 import axios from "axios";
 import Button from "../components/Button";
+import DefaultPicture from "../assets/default_user.jpg";
 
 const Profile = () => {
   const [user, setUser] = useState(null);
@@ -17,20 +26,20 @@ const Profile = () => {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const loggedInUser = await checkIfLoggedIn();
+        const { ok, user } = await checkIfLoggedIn();
         const userId = new URLSearchParams(location.search).get("id");
 
         if (userId) {
-          const response = await axios.get(`${API_URL}/getUser?id=${userId}`);
-          if (response.data.ok && response.data.user) {
-            setUser(response.data.user);
-            setIsAltUser(response.data.user._id !== loggedInUser.user._id);
+          const { data } = await axios.get(`${API_URL}/getUser?id=${userId}`);
+          if (data.ok && data.user) {
+            setUser(data.user);
+            setIsAltUser(data.user._id !== user._id);
           } else {
             navigate("/login");
           }
         } else {
-          if (loggedInUser.ok && loggedInUser.user) {
-            setUser(loggedInUser.user);
+          if (ok && user) {
+            setUser(user);
             setIsAltUser(false);
           } else {
             navigate("/login");
@@ -47,8 +56,8 @@ const Profile = () => {
     setIsLoading(true);
     setError("");
     try {
-      const response = await logout();
-      if (response.success) {
+      const { success } = await logout();
+      if (success) {
         toast.success("Conta deslogada com sucesso.");
         setTimeout(() => navigate("/"), 2000);
       } else {
@@ -63,11 +72,7 @@ const Profile = () => {
   };
 
   if (!user) {
-    return (
-      <section className="h-full w-full flex justify-center items-center">
-        <p>Carregando...</p>
-      </section>
-    );
+    navigate("/login");
   }
 
   const formatDate = (date) =>
@@ -79,7 +84,8 @@ const Profile = () => {
       minute: "2-digit",
       timeZone: "UTC",
     }) || "Data inválida";
-
+  const username =
+    user.username[0].toUpperCase() + user.username.slice(1) || "N/A";
   const userInfo = [
     { icon: <User />, label: "Nome", value: user.username },
     { icon: <Mail />, label: "Email", value: user.email || "N/A" },
@@ -104,28 +110,20 @@ const Profile = () => {
 
   return (
     <section className="h-full w-full flex justify-center items-center relative">
-      <div className="flex flex-col items-left gap-3 shadow-md rounded-xl p-4 bg-white">
-        <h1 className="text-5xl font-oswald font-bold text-center">
-          INFORMAÇÕES DO USUÁRIO
-        </h1>
-        {userInfo.map(({ icon, label, value }) => (
-          <div key={label} className="flex items-center gap-2">
-            {icon}{" "}
-            <p>
-              {label}: <strong>{value}</strong>
-            </p>
+      <div className="w-full max-w-4xl p-4 bg-white rounded-lg shadow-lg flex justify-between items-center gap-6">
+        <div className="flex gap-4 items-center justify-center">
+          <div
+            id="profile-picture"
+            className="rounded-full w-16 h-16 bg-gray-200 flex justify-center items-center">
+            <img src={DefaultPicture} className="object-cover rounded-full" />
           </div>
-        ))}
-        {error && <p className="text-sm text-red-600">{error}</p>}
-        {!isAltUser && (
-          <div className="flex justify-center mt-4">
-            <Button
-              label={isLoading ? "Deslogando..." : "Deslogar"}
-              click={handleLogout}
-              disabled={isLoading}
-            />
+          <div>
+            <h1 className="text-2xl font-bold mt-4">{username}</h1>
+            <p className="text-sm text-gray-500 ">{user.email}</p>
           </div>
-        )}
+        </div>
+        {error && error}
+        <Button label="Sair" icon={<CornerUpLeft />} click={handleLogout} />
       </div>
     </section>
   );
